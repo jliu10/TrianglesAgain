bcaedb0c6300a602ac1ac4d167a2c894cbc01ba6
jliu10 <jliu10@stuy.edu>
2020-10-28 17:59:14 -0400

    created class Tester, tested methods getX and getY

---
+++ Tester.java
@@ -0,0 +1,8 @@
+public class Tester{
+  public static void main(String[] args){
+    Point z=new Point(0,0);
+    Point a=new Point(1,2);
+    System.out.println(a.getX());
+    System.out.println(a.getY());
+  }
+}
