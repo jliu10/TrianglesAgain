8324f1f71fd39f3e243b49954c57f0e2299ce4af
jliu10 <jliu10@stuy.edu>
2020-10-28 19:10:54 -0400

    tested method classify

--- Tester.java
+++ Tester.java
@@ -2,6 +2,7 @@ public class Tester{
   public static void main(String[] args){
     Point z=new Point(0,0);
     Point a=new Point(1,2);
+    Point b=new Point(3,0);
     Point a1=new Point(a);
     System.out.println(a.getX());
     System.out.println(a.getY());
@@ -9,5 +10,12 @@ public class Tester{
     System.out.println(a.distanceTo(a));
     System.out.println(a.equals(z));
     System.out.println(a.equals(a1));
+    //Triangle
+    Triangle bob=new Triangle(z,a,b);
+    Triangle isos=new Triangle(0,0, 1,1, 2,0);
+    System.out.println(bob.getPerimeter());
+    System.out.println(bob.getArea());
+    System.out.println(bob.classify());
+    System.out.println(isos.classify());
   }
 }
