b69498c14a631a4574c673334bdc6af3add29280
jliu10 <jliu10@stuy.edu>
2020-10-28 18:20:24 -0400

    tested method equals

--- Tester.java
+++ Tester.java
@@ -2,9 +2,12 @@ public class Tester{
   public static void main(String[] args){
     Point z=new Point(0,0);
     Point a=new Point(1,2);
+    Point a1=new Point(a);
     System.out.println(a.getX());
     System.out.println(a.getY());
     System.out.println(a.distanceTo(z));
     System.out.println(a.distanceTo(a));
+    System.out.println(a.equals(z));
+    System.out.println(a.equals(a1));
   }
 }
